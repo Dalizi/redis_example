b3086dd1bfc5f7543130f4f1b470ed7d73b76786
谭泽寰 <tanzehuan@cofco.com>
2016-01-26 17:01:51 +0800

    Newly added

---
+++ QoutesExample/DataSetQuotes.cs
@@ -0,0 +1,91 @@
+﻿using StackExchange.Redis;
+using System.Data;
+using System;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace QuotesExample
+{
+
+
+    partial class DataSetQuotes
+    {
+        void InitializeData()
+        {
+            conn = ConnectionMultiplexer.Connect("10.2.7.210:6379,password=Finders6");
+            ISubscriber sub= conn.GetSubscriber();
+            sub.Subscribe("SR605", new Action<RedisChannel, RedisValue>(DoSomeThing));
+        }
+
+        private void DoSomeThing(RedisChannel channel, RedisValue message)
+        {
+            DataRow row;
+            DataTable quotesTable = this.Tables[0];
+            row = quotesTable.Rows.Find(channel.ToString());
+            if (row == null)
+            {
+                row = quotesTable.NewRow();
+                row["ContractCode"] = channel.ToString();
+                try
+                {
+                    foreach (String item in ((string)message).Split(','))
+                    {
+                        String key;
+                        String value;
+                        String[] keyValue = item.Split(':');
+                        key = keyValue[0];
+                        value = keyValue[1];
+                        if (quotesTable.Columns[key].DataType == typeof(String))
+                        {
+                            row[key] = value;
+                        }
+                        else if (quotesTable.Columns[key].DataType == typeof(decimal))
+                        {
+                            row[key] = Convert.ToDecimal(value);
+                        }
+                    }
+
+                    quotesTable.Rows.Add(row);
+
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Exception");
+                }
+            }
+            else
+            {
+                try
+                {
+                    foreach (String item in ((string)message).Split(','))
+                    {
+                        String key;
+                        String value;
+                        String[] keyValue = item.Split(':');
+                        key = keyValue[0];
+                        value = keyValue[1];
+                        if (quotesTable.Columns[key].DataType == typeof(String))
+                        {
+                            row[key] = value;
+                        }
+                        else if (quotesTable.Columns[key].DataType == typeof(decimal))
+                        {
+                            row[key] = Convert.ToDecimal(value);
+                        }
+                        else if (quotesTable.Columns[key].DataType == typeof(DateTime))
+                        {
+                            row[key] = DateTime.ParseExact(value, "HHmmssfff", CultureInfo.InvariantCulture);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Exception");
+                }
+            }
+        }
+
+        ConnectionMultiplexer conn;
+    }
+}
---
+++ QoutesExample/FormQuotes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using StackExchange.Redis;
+
+namespace QuotesExample
+{
+    public partial class FormQuotes : Form
+    {
+        public FormQuotes()
+        {
+            InitializeComponent();
+           
+        }
+
+
+       
+        public DataTable quotesTable;
+        ConnectionMultiplexer conn;
+
+        private void dataTableMarketQuotesBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            this.dataGridViewQuotes.Refresh();
+        }
+    }
+}
